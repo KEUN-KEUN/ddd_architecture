b5d9a39c1fdd5dcaf750542ac0077af9232bbdd6
KEUN <keun90@sae-a.com>
2025-09-16 11:01:48 +0900

    messageBus 구현 중 flow: messageBus > handler > UoiW ...

--- src/allocation/service_layer/unit_of_work.py
+++ src/allocation/service_layer/unit_of_work.py
@@ -17,8 +17,16 @@ class AbstractUnitOfWork(abc.ABC):
     def __exit__(self, *args):
         self.rollback()
 
-    @abc.abstractmethod
     def commit(self):
+        self._commit()
+        
+    def collect_new_events(self):
+        for product in self.products.seen:
+            while product.events:
+                yield product.events.pop(0)
+
+    @abc.abstractmethod
+    def _commit(self):
         raise NotImplementedError
 
     @abc.abstractmethod
@@ -40,17 +48,19 @@ class SqlAlchemyUnitOfWork(AbstractUnitOfWork):
 
     def __enter__(self):
         self.session: Session = self.session_factory()
-        self.products = repository.SqlAlchemyRepository(self.session)    
+        self.products = repository.TrackingRepository(
+            repository.SqlAlchemyRepository(self.session)    
+        )
         return super().__enter__()
 
     def __exit__(self, *args):
         super().__exit__(*args)
         self.session.close()
     
-    def commit(self):
+    def _commit(self):
         self.session.commit()
 
     def rollback(self): 
-        self.session
+        self.session.rollback()
 
 
